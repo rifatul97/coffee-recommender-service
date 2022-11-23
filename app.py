05703f6cb8d2b875e8d579465200af5431cbffd8
rifatul97 <rifatulkarim1997@example.com>
2022-11-22 21:58:09 -0500

    added new api route

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "hello from flask :-)"
+    return "welcome to nmf service"
+
+
+@app.route('/get_recommend_for')
+def recommend():
+    args = []
+    for arg in request.args.to_dict().values():
+        args.append(arg + " ")
+
+    return ''.join(map(str, args));
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
