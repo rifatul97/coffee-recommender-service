20f39fc98c87c730406cf8506c9270bc91489ba0
rifatul97 <rifatulkarim1997@example.com>
2022-11-23 13:31:58 -0500

    modified program to read file upload from my github repo

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
+import requests
+import io
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF
+from pandas import pandas as pd
 
 app = Flask(__name__)
 model = None
 tfidfVect = None
-file_name = 'coffee_reviews_cleaned.txt'
+file_url = "https://raw.githubusercontent.com/rifatul97/nmfService/main/coffee_reviews_cleaned.txt"
 coffee_roasters = []
 coffee_reviews = []
 
@@ -26,16 +31,20 @@ def recommend():
 
 @app.route('/get_number_of_coffee_reviews', methods=['GET'])
 def get_number_of_coffee_reviews():
-    return str(len(coffee_reviews))
+    number_of_coffee_review_text = "number of coffee reviews : " + str(len(coffee_reviews))
+    number_of_coffee_roaster_text = "number of coffee roasters : " + str(len(coffee_roasters))
+    return number_of_coffee_roaster_text + "\n" + number_of_coffee_review_text
 
 
 def readCoffeeReviewData():
-    file = open(file_name, 'r')
-    for line in file.readlines():
+
+    file_download = requests.get(file_url).content
+    coffee_review_data = (io.StringIO(file_download.decode('utf-8')))
+
+    for line in coffee_review_data.readlines():
         coffeeReview = line.rstrip().split(',')
         coffee_roasters.append(coffeeReview[0])
         coffee_reviews.append(coffeeReview[1])
-
 
 if __name__ == '__main__':
     readCoffeeReviewData()
