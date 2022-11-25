08ee595a9fd97753ad8bb21e3e98e29aca391e0b
rifatul97 <rifatulkarim1997@example.com>
2022-11-24 21:56:03 -0500

    prevented re-adding existing data to redis

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF
 from pandas import pandas as pd
 
+
+def get_redis_url():
+    if Path('properties').exists():
+        with open("properties") as f:
+            read_line = [line.split("=") for line in f.readlines()]
+            key_value_pair = {key.strip(): value.strip() for key, value in read_line}
+            return key_value_pair['coffee_redis_key']
+    else:
+        url = os.environ.get('coffee_redis_key')
+        print("url = " + url)
+        return url
+
+
 app = Flask(__name__)
+r = redis.from_url(get_redis_url())
 model = None
 tfidfVect = None
 file_url = "https://raw.githubusercontent.com/rifatul97/coffee-recommender-service/main/data/coffee_reviews_cleaned.txt"
@@ -35,40 +49,35 @@ def recommend():
 
 @app.route('/get_number_of_coffee_reviews', methods=['GET'])
 def get_number_of_coffee_reviews():
-    unpacked_json = json.loads(r.get('coffee_reviews_json'))
+    unpacked_json = r.get('coffee_reviews_json').decode('utf-8')
+    unpacked_json_len = len(unpacked_json)
+    print(unpacked_json[unpacked_json_len-1])
+    load_unpacked_json = json.loads(unpacked_json)
 
-    number_of_coffee_review_text = "number of coffee reviews : " + str(len(unpacked_json))
-    number_of_coffee_roaster_text = "" #"number of coffee roasters : " + str(len(coffee_roasters))
+    number_of_coffee_review_text = "number of coffee reviews : " + str(len(load_unpacked_json))
+    number_of_coffee_roaster_text = ""  # "number of coffee roasters : " + str(len(coffee_roasters))
     return number_of_coffee_roaster_text + "\n" + number_of_coffee_review_text
 
 
 def readCoffeeReviewData():
-    coffee_reviews = []
-    coffee_roaster = []
-    file_download = requests.get(file_url).content
-    coffee_review_data = (io.StringIO(file_download.decode('utf-8')))
-    for line in coffee_review_data.readlines():
-        coffeeReview = line.rstrip().split(',')
-        # coffee_roasters.append(coffeeReview[0])
-        coffee_reviews.append(coffeeReview[1])
+    # print(len(r.get('coffee_reviews_json')))
+    get_coffee_review_cache = r.get('coffee_reviews_json').decode('utf-8');
+    if len(get_coffee_review_cache) == 0:
+        coffee_reviews = []
+        coffee_roaster = []
+        file_download = requests.get(file_url).content
+        coffee_review_data = (io.StringIO(file_download.decode('utf-8')))
+        for line in coffee_review_data.readlines():
+            coffeeReview = line.rstrip().split(',')
+            # coffee_roasters.append(coffeeReview[0])
+            coffee_review = coffeeReview[1].strip()
+            # print(coffee_review)
+            coffee_reviews.append(coffee_review)
 
-    coffee_reviews_json = json.dumps(coffee_reviews)
-    r.append('coffee_reviews_json', coffee_reviews_json)
-
-
-def get_redis_url():
-    if Path('properties').exists():
-        with open("properties") as f:
-            read_line = [line.split("=") for line in f.readlines()]
-            key_value_pair = {key.strip(): value.strip() for key, value in read_line}
-            return key_value_pair['coffee_redis_key']
-    else:
-        url = os.environ.get('coffee_redis_key')
-        print("url = " + url)
-        return url
+        r.append('coffee_reviews_json', json.dumps(coffee_reviews))
 
 
 if __name__ == '__main__':
-    r = redis.from_url(get_redis_url())
+    # r.delete('coffee_reviews_json')
     readCoffeeReviewData()
     app.run()
