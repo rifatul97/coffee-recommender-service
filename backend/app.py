716b44be39350f3e6f459c4a3947ea63419648c4
rifatul97 <rifatulkarim1997@example.com>
2022-11-26 20:22:35 -0500

    removed the comments of the app.py

--- backend/app.py
+++ backend/app.py
@@ -60,7 +60,6 @@ def get_number_of_coffee_reviews():
 
 
 def readCoffeeReviewData():
-    # print(len(r.get('coffee_reviews_json')))
     get_coffee_review_cache = r.get('coffee_reviews_json').decode('utf-8');
     if len(get_coffee_review_cache) == 0:
         coffee_reviews = []
@@ -69,15 +68,12 @@ def readCoffeeReviewData():
         coffee_review_data = (io.StringIO(file_download.decode('utf-8')))
         for line in coffee_review_data.readlines():
             coffeeReview = line.rstrip().split(',')
-            # coffee_roasters.append(coffeeReview[0])
             coffee_review = coffeeReview[1].strip()
-            # print(coffee_review)
             coffee_reviews.append(coffee_review)
 
         r.append('coffee_reviews_json', json.dumps(coffee_reviews))
 
 
 if __name__ == '__main__':
-    # r.delete('coffee_reviews_json')
     readCoffeeReviewData()
     app.run()
